8defebf84ab39fd2fb6d02317136986ca1c172d9
Ryohei <harrry.kuma1225@gmail.com>
2023-04-26 21:48:19 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-
+import { Card } from './component/card/Card';
+import { Navbar } from './component/Navbar/Navbar';
+import { getAllPokemon, getPokemon } from './utils/pokenmon';
 function App() {
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+  const [loading,setloading] = useState(true);
+  const [pokemonDate,setpokemondate] =useState([])
+  const [nextURL,setnextURL] =useState('')
+  const [prevURL,setprevURL] =useState('')
+  useEffect(() =>{
+    const fetchPokemonDate = async() =>{
+       let res = await getAllPokemon(initialURL);
+      console.log(res)
+      loadPokemon(res.results);
+      setnextURL(res.next)
+      setprevURL(res.previous)
+       setloading(false);
+    }
+    fetchPokemonDate();
+  },[])
+const loadPokemon = async(data) =>{
+let _pokemonDate =await Promise.all(data.map((pokemon) =>{
+  let pokemonRecord = getPokemon(pokemon.url);
+  return pokemonRecord;
+}));
+setpokemondate(_pokemonDate);
+}
+const handleNext = async() =>{
+setloading(true);
+let date = await getAllPokemon(nextURL);
+await loadPokemon(date.results);
+setnextURL(date.next);
+setprevURL(date.previous);
+setloading(false)
+}
+const handlePrev =async () =>{
+if(!prevURL)return;
+setloading(true);
+let date = await getAllPokemon(prevURL);
+await loadPokemon(date.results)
+setnextURL(date.next);
+setprevURL(date.previous);
+setloading(false);
+}
+
   return (
+    <>
+    <Navbar/>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (<h1>ロード中</h1>):
+    ( <>
+    <div className="pokemonCardContainer">
+      {pokemonDate.map((pokemon,i) =>{
+        return<Card key={i} pokemon={pokemon}/>
+      })}
     </div>
+    <div className="btn">
+      <button onClick={handlePrev}>前</button>
+      <button onClick={handleNext}>後</button>
+    </div>
+      </> )} 
+    </div>
+      </>
   );
 }
 
